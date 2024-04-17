b9b9b24b987ac7f0115d20e93720bd99675cec17
unknown <romainparisot.pro@gmail.com>
2024-04-17 11:59:07 +0200

    feat: delete task

--- js/App.js
+++ js/App.js
@@ -1,5 +1,6 @@
 import TaskManager, { Priority } from './Task.js';
 var taskManager = new TaskManager();
+// Add a new task
 var form = document.querySelector('#taskForm');
 if (form) {
     form.addEventListener('submit', function (event) {
@@ -11,6 +12,7 @@ if (form) {
         var date = formData.get('date');
         var priority = formData.get('priority');
         var category = formData.get('category');
+        // if not empty
         if (title && description && date && priority && category) {
             taskManager.addTask({
                 title: title.toString(),
@@ -23,16 +25,34 @@ if (form) {
             // cerate the task
             var taskElement = document.createElement('div');
             taskElement.className = "task ".concat(priority.toString().toLowerCase());
-            taskElement.innerHTML = "\n              <h3>".concat(title, " <span>\u2013 Priorit\u00E9 ").concat(Priority[priority.toString()], "</span></h3>\n              <p>Cat\u00E9gorie: ").concat(category, "</p>\n              <p>Date d'\u00E9ch\u00E9ance: ").concat(date, "</p>\n              <p>").concat(description, "</p>\n              <button type=\"button\">Supprimer</button>\n              <button class=\"edit-btn\">Modifier</button>\n            ");
-            var taskList = document.getElementById('tasks');
-            if (taskList) {
-                taskList.appendChild(taskElement);
+            taskElement.innerHTML = "\n        <h3>".concat(title, " <span>\u2013 Priorit\u00E9 ").concat(Priority[priority.toString()], "</span></h3>\n        <p>Cat\u00E9gorie: ").concat(category, "</p>\n        <p>Date d'\u00E9ch\u00E9ance: ").concat(date, "</p>\n        <p>").concat(description, "</p>\n        <button data-index=\"").concat(taskManager.tasks.length - 1, "\" type=\"button\">Supprimer</button>\n        <button class=\"edit-btn\">Modifier</button>\n      ");
+            var taskList_1 = document.getElementById('tasks');
+            if (taskList_1) {
+                taskList_1.appendChild(taskElement);
             }
             // reset from after submit
             form.reset();
         }
         else {
             alert('Veuillez remplir tous les champs');
+        }
+    });
+}
+// Remove a task
+var taskList = document.getElementById('tasks');
+if (taskList) {
+    taskList.addEventListener('click', function (event) {
+        var target = event.target;
+        // if it is a delete button
+        if (target && target.dataset.index) {
+            // get index of the task
+            var taskIndex = Number(target.dataset.index);
+            // call deleteTask
+            taskManager.deleteTask(taskIndex);
+            // remove from dom
+            if (target.parentElement) {
+                taskList.removeChild(target.parentElement);
+            }
         }
     });
 }
--- js/App.ts
+++ js/App.ts
@@ -1,13 +1,16 @@
 import TaskManager, { Priority } from './Task.js';
 const taskManager = new TaskManager();
 
+
+
+// Add a new task
+
 const form = document.querySelector('#taskForm');
 
 if (form) {
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log('submit');
-    
 
     const formData = new FormData(form as HTMLFormElement);
 
@@ -17,6 +20,7 @@ if (form) {
     const priority = formData.get('priority');
     const category = formData.get('category');
 
+    // if not empty
     if (title && description && date && priority && category) {
       taskManager.addTask({
         title: title.toString(),
@@ -27,22 +31,22 @@ if (form) {
       });
 
       console.log(taskManager.tasks);
-            // cerate the task
-            const taskElement = document.createElement('div');
-            taskElement.className = `task ${priority.toString().toLowerCase()}`;
-            taskElement.innerHTML = `
-              <h3>${title} <span>– Priorité ${Priority[priority.toString() as keyof typeof Priority]}</span></h3>
-              <p>Catégorie: ${category}</p>
-              <p>Date d'échéance: ${date}</p>
-              <p>${description}</p>
-              <button type="button">Supprimer</button>
-              <button class="edit-btn">Modifier</button>
-            `;
+      // cerate the task
+      const taskElement = document.createElement('div');
+      taskElement.className = `task ${priority.toString().toLowerCase()}`;
+      taskElement.innerHTML = `
+        <h3>${title} <span>– Priorité ${Priority[priority.toString() as keyof typeof Priority]}</span></h3>
+        <p>Catégorie: ${category}</p>
+        <p>Date d'échéance: ${date}</p>
+        <p>${description}</p>
+        <button data-index="${taskManager.tasks.length - 1}" type="button">Supprimer</button>
+        <button class="edit-btn">Modifier</button>
+      `;
 
-            const taskList = document.getElementById('tasks');
-            if (taskList) {
-              taskList.appendChild(taskElement);
-            }
+      const taskList = document.getElementById('tasks');
+      if (taskList) {
+        taskList.appendChild(taskElement);
+      }
 
       // reset from after submit
       (form as HTMLFormElement).reset();
@@ -55,7 +59,27 @@ if (form) {
 }
 
 
+// Remove a task
 
+const taskList = document.getElementById('tasks');
+
+if (taskList) {
+  taskList.addEventListener('click', (event) => {
+    const target = event.target as HTMLElement;
+
+    // if it is a delete button
+    if (target && target.dataset.index) {
+      // get index of the task
+      const taskIndex = Number(target.dataset.index);
+      // call deleteTask
+      taskManager.deleteTask(taskIndex);
+      // remove from dom
+      if (target.parentElement) {
+        taskList.removeChild(target.parentElement);
+      }
+    }
+  });
+}
 
 
 
